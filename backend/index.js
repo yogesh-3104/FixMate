cb01af3198b3b9b73f149b90835e823601721f65
yogesh-3104 <yg4977428@gmail.com>
2025-06-17 22:54:21 +0530

    Fix CORS to allow Vercel frontend

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:[ "http://localhost:5173","https://fixmate-six.vercel.app"] ,
     credentials: true,
   })
 );
